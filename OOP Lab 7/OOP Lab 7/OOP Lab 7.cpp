3fd3353b090b74fec16910741dbf3491154bb8fe
KTYDG <molokanov02@gmail.com>
2021-11-20 11:16:38 +0300

    ignore
    ignore

--- OOP Lab 7/OOP Lab 7/OOP Lab 7.cpp
+++ OOP Lab 7/OOP Lab 7/OOP Lab 7.cpp
@@ -7,66 +7,114 @@
 #include <set>
 using namespace std;
 
+struct Leaks {
+    ~Leaks() { _CrtDumpMemoryLeaks(); }
+} _leak;
+
 class Goods {
 public:
-    int *amount;
-    virtual int Get() = 0;
+    int* amount;
+    wstring* name;
+    virtual int GetAmount() = 0;
+    virtual wstring GetName() = 0;
     virtual ~Goods() = default;
 };
 
 class TShirt: public Goods {
 public:
-    TShirt(int a) { amount = new int(a); }
-    virtual int Get() { return *amount; }
-    virtual ~TShirt() { delete amount; }
+    TShirt(int a, wstring n) { 
+        amount = new int(a); 
+        name = new wstring(n);
+    }
+    virtual int GetAmount() { return *amount; }
+    virtual wstring GetName() { return *name; }
+    virtual ~TShirt() {
+        delete amount;
+        delete name;
+    }
 };
 class Cap: public Goods {
 public:
-    Cap(int a) { amount = new int(a); }
-    virtual int Get() { return *amount; }
-    virtual ~Cap() { delete amount; }
+    Cap(int a, wstring n) {
+        amount = new int(a);
+        name = new wstring(n);
+    }
+    virtual int GetAmount() { return *amount; }
+    virtual wstring GetName() { return *name; }
+    virtual ~Cap() {
+        delete amount;
+        delete name;
+    }
 };
 class Ball: public Goods {
 public:
-    Ball(int a) { amount = new int(a); }
-    virtual int Get() { return *amount; }
-    virtual ~Ball() { delete amount; }
+    Ball(int a, wstring n) {
+        amount = new int(a);
+        name = new wstring(n);
+    }
+    virtual int GetAmount() { return *amount; }
+    virtual wstring GetName() { return *name; }
+    virtual ~Ball() {
+        delete amount;
+        delete name;
+    }
 };
 class Backpack: public Goods {
 public:
-    Backpack(int a) { amount = new int(a); }
-    virtual int Get() { return *amount; }
-    virtual ~Backpack() { delete amount; }
+    Backpack(int a, wstring n) {
+        amount = new int(a);
+        name = new wstring(n);
+    }
+    virtual int GetAmount() { return *amount; }
+    virtual wstring GetName() { return *name; }
+    virtual ~Backpack() {
+        delete amount;
+        delete name;
+    }
 };
 class Boots: public Goods {
 public:
-   Boots(int a) { amount = new int(a); }
-   virtual int Get() { return *amount; }
-    virtual ~Boots() { delete amount; }
+   Boots(int a, wstring n) {
+       amount = new int(a);
+       name = new wstring(n);
+   }
+   virtual int GetAmount() { return *amount; }
+   virtual wstring GetName() { return *name; }
+   virtual ~Boots() {
+       delete amount;
+       delete name;
+   }
 };
 class Byke: public Goods {
 public:
-    Byke(int a) { amount = new int(a); }
-    int Get() { return *amount; }
-    ~Byke() { delete amount; }
+    Byke(int a, wstring n) {
+        amount = new int(a);
+        name = new wstring(n);
+    }
+    int GetAmount() { return *amount; }
+    wstring GetName() { return *name; }
+    ~Byke() {
+        delete amount;
+        delete name;
+    }
 };
 
 vector<Goods*>v;
 
 void bigIF(wstring type, int amount) {
-    if(type == L"футболка") { v.push_back(new TShirt(amount)); return; }
-    if(type == L"бейсболка") { v.push_back(new Cap(amount)); return; }
-    if(type == L"мяч") { v.push_back(new Ball(amount)); return; }
-    if(type == L"рюкзак") { v.push_back(new Backpack(amount)); return; }
-    if(type == L"кеды") { v.push_back(new Boots(amount)); return; }
-    if(type == L"велосипед") { v.push_back(new Byke(amount)); return; }
+    if(type == L"футболка") { v.push_back(new TShirt(amount, type)); return; }
+    if(type == L"бейсболка") { v.push_back(new Cap(amount, type)); return; }
+    if(type == L"мяч") { v.push_back(new Ball(amount, type)); return; }
+    if(type == L"рюкзак") { v.push_back(new Backpack(amount, type)); return; }
+    if(type == L"кеды") { v.push_back(new Boots(amount, type)); return; }
+    if(type == L"велосипед") { v.push_back(new Byke(amount, type)); return; }
 }
 
 int main() {
     locale::global(locale("ru_RU.UTF-8"));
     const locale utf8_locale = locale(locale(), new codecvt_utf8<wchar_t>());
 
-    multimap<set<int>, Goods*>list;
+    multimap<set<int>, wstring>list;
     wstring type;
     int amount;
     // Чтение файла и заполение вектора
@@ -85,8 +133,12 @@ int main() {
     // Заполнение multimap
     for(int i = 0; i < v.size(); i++) {
         set<int>inside; // Создание контейнера set
-        inside.insert(v[i]->Get()); // Положили в set количество товара
-        list.emplace(inside, v[i]); // Создаем элемент контейнера multimap
+        inside.insert(v[i]->GetAmount()); // Положили в set количество товара
+        list.emplace(inside, v[i]->GetName()); // Создаем элемент контейнера multimap
+    }
+
+    for(int i = 0; i < v.size(); i++) {
+        delete v[i];
     }
     return 0;
 }
