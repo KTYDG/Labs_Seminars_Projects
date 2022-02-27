05c71b02c2240ef5b244261965f5049a5a862ed4
KTYDG <molokanov02@gmail.com>
2022-02-28 00:12:42 +0300

    сделана третья лаба

--- Molokanov/OOP/OOP Lab 3 Sem2/OOP Lab 3 Sem2/OOP Lab 3 Sem2.cpp
+++ Molokanov/OOP/OOP Lab 3 Sem2/OOP Lab 3 Sem2/OOP Lab 3 Sem2.cpp
@@ -1,29 +1,41 @@
 ﻿#include "pch.h"
 
-void OpenFile() {
+
+struct comparator {
+	bool operator()(wstring s1, wstring s2) {
+		return s1 < s2;
+	}
+} compare;
+
+void OpenFile(list<wstring>&source) {
 	const locale utf8_locale = locale(locale(), new codecvt_utf8<wchar_t>());
 
 	wstring s = L"";
-	wifstream input;
 
-	if(number == 362) input.open("362.txt");
-	else if(number == 112) input.open("112.txt");
+	wifstream input;
+	input.open("article.txt");
 	if(!input) {
-		cout << "�� ������� ������� ����" << endl;
+		cout << "Не удалось открыть файл" << endl;
 		return;
 	}
 	input.imbue(utf8_locale);
 	while(!input.eof()) {
 		getline(input, s, L'\n');
-		if(!s.empty()) storage.push_back(new Type(s));
+		if(!s.empty()) source.push_back(s);
 	}
-	sort(storage.begin(), storage.end(), compare);
+	source.sort(compare);
 }
 
-
-
-
+struct Leaks {
+	~Leaks() { _CrtDumpMemoryLeaks(); }
+} leaks;
 
 int main() {
-	
+	locale::global(locale("ru_RU.UTF-8"));
+
+	list<wstring>source;
+
+	OpenFile(source);
+
+	return 0;
 }
